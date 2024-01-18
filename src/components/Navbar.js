c2e84748498ab12d5a8c7fb73a26fb5d2134a26f
vignesh <vigneshwar.sigamani@gmail.com>
2024-01-18 16:36:38 +0530

    add items to the cart

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,17 +1,22 @@
 import {Button, Container, Navbar, Modal} from 'react-bootstrap';
-import { useState } from 'react';
+import { useState, useContext } from 'react';
+import { CartContext } from '../CartContext';
+
 function NavbarComponent(){
+    const cart = useContext(CartContext);
+
     const [show, setshow] = useState(false);
     const handleClose = () => setshow(false); // when the hide event happens we will hide the modal 
     const handleShow = () => setshow(true);
 
+    const productsCount = cart.items.reduce((sum,product) => sum + product.quantity,0);
     return(
     <div>
     <Navbar expand="sm">
         <Navbar.Brand href='/'>Music Shopping</Navbar.Brand>
         <Navbar.Toggle/>
         <Navbar.Collapse className="justify-content-end">
-            <Button onClick={handleShow}>Cart 0 Items</Button>
+            <Button onClick={handleShow}>Cart ({productsCount} Items)</Button>
         </Navbar.Collapse>
     </Navbar>
     <Modal show={show} onHide={handleClose}>
@@ -19,7 +24,19 @@ function NavbarComponent(){
             <Modal.Title>Shopping Cart</Modal.Title>
         </Modal.Header>
         <Modal.Body>
-            <h1>This is the modal body</h1>
+            {productsCount > 0 ?
+            <div>
+                <p>Items in your cart</p>
+                {cart.items.map((currentProduct,index) => (
+                    <h1>{currentProduct.id}</h1>
+                ))}
+                <h1>Total : {cart.getTotalCost()}</h1>
+                <Button variant='success'>Purchase Items</Button>
+            </div>
+            :
+            <h1>This is no items in your cart</h1>
+            }
+            
         </Modal.Body>
     </Modal>
     </div>
