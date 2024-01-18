e18d9425b183d6e5e423c4e430fa7a31d8452f36
vignesh <vigneshwar.sigamani@gmail.com>
2024-01-18 13:45:59 +0530

    add context , blueprint of context and get product quantity

--- src/CartContext.js
+++ src/CartContext.js
@@ -12,9 +12,46 @@ import { productsArray } from "./productsStore";
 //initilise the context by creating an object 
 /**
  * 1. We dont define the functions right inside the create context , this context is saying that we can define a function for getproductquantity
+ * when we create the getproductquantity function , we can pass that to the provider 
  */
-const createContext = createContext({
+
+// giving our application access to the context , we will export
+
+export const createContext = createContext({
     items:[],
     getProductQuantity:() => {},
-
+    addOneToCart:() => {},
+    removeOneFromCart:() => {},
+    deleteAllFromCart:() => {},
+    getTotalCost:() => {}
 });
+
+
+// provider => anything inside the cartprodivder is the children
+export function CartProvider({children}){
+    const [cartProducts,setCartProducts] = useState([]); // we will udpate the setCardProducts array and then pass it as an item
+
+
+    function getProductQuantity(id){
+        const quantity = cartProducts.find(product => product.id === id)?.quantity;
+
+        if(quantity === undefined) return 0;
+
+        return quantity;
+    }
+    
+    const contextValue = {
+        items:cartProducts,
+        getProductQuantity,
+        addOneToCart,
+        removeOneFromCart,
+        deleteAllFromCart,
+        getTotalCost
+    }
+    return(
+        <CartProvider value={contextValue}>
+            {children}
+        </CartProvider>
+    )
+
+}
